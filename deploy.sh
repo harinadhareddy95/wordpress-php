390d18d6599ad5df7f61d96c7ddb6aa699193f29
harinadhareddy95 <131929612+harinadhareddy95@users.noreply.github.com>
2023-05-15 17:39:14 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 unzip wordpress.zip
-sudo rm -fr /var/www/html/ *
 sudo cp -r * /var/www/html
-rm -rf * .zip *.php wp-admin wp-content wp-includes *.html
+rm -rf *.zip  wp-admin wp-content wp-includes *.html
